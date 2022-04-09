1da432faa8794978c0fa22cc65983d478d2d84fa
tubond <tybond2003@ya.ru>
2022-04-09 21:06:10 +0300

    сыро, в процессе

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -12,10 +12,24 @@ import io.socket.client.IO;
 import io.socket.client.Socket;
 
 import android.content.SharedPreferences;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
 
 public class MainActivity extends AppCompatActivity {
     private String room = "null";
+
+    private Button changeRoomBut;
+    private TextView coordsText;
+    private TextView roomText;
+
+    private MapView mapView;
 
     private Socket mSocket;
     {
@@ -25,18 +39,35 @@ public class MainActivity extends AppCompatActivity {
         catch (URISyntaxException ignored) {}
     }
 
+    private String APP_PREFERENCES = "usersettings";
+    private SharedPreferences mSettings;
+    private String APP_PREFERENCES_ROOM = "room";
+
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey("0f07d937-a358-4269-836d-33d9285feea5");
+        MapKitFactory.initialize(this);
         setContentView(R.layout.activity_main);
 
-        String APP_PREFERENCES = "usersettings";
-        SharedPreferences mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        //SearchFactory.initialize(this);
 
 
-        String APP_PREFERENCES_ROOM = "room";
+        changeRoomBut = findViewById(R.id.buttonChangeRoom);
+        coordsText = findViewById(R.id.TextCoords);
+        roomText = findViewById(R.id.TextCurRoom);
+
+        mapView = findViewById(R.id.mapView_);
+        mapView.getMap()
+                .move(new CameraPosition(new Point(55.648100, 37.652216), 15.0f, 0.0f, 0.0f),
+                        new Animation(Animation.Type.SMOOTH, 0), null);
+
+        //mapView.getMap().addCameraListener(Context);
+
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+
         if(mSettings.contains(APP_PREFERENCES_ROOM)) {
             room = mSettings.getString(APP_PREFERENCES_ROOM, "null");
         }
@@ -46,8 +77,26 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+        MapKitFactory.getInstance().onStart();
+
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        if(mSettings.contains(APP_PREFERENCES_ROOM)) {
+            room = mSettings.getString(APP_PREFERENCES_ROOM, "null");
+        }
+
+        roomText.setText(room);
+
+        changeRoomBut.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+            startActivity(intent);
+        });
+
         mSocket.connect();
-
-
     }
 }
--- app/src/main/java/com/example/app/RegistrationActivity.java
+++ app/src/main/java/com/example/app/RegistrationActivity.java
@@ -1,9 +1,13 @@
 package com.example.app;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 //import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -12,15 +16,44 @@ public class RegistrationActivity extends AppCompatActivity {
     private Button subBut;
     private EditText roomEdit;
     private TextView infoText;
-
+    private String APP_PREFERENCES = "usersettings";
+    private String APP_PREFERENCES_NEW_ROOM = "room";
+    private SharedPreferences mSettings;
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_reg);
+
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+
         subBut = findViewById(R.id.submit_reg);
         roomEdit = findViewById(R.id.editRoom);
         infoText = findViewById(R.id.textView);
 
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        subBut.setOnClickListener(v -> {
+            String bufStr = roomEdit.getText().toString();
+            if(bufStr.length() < 5) {
+                Toast.makeText(this, "input error",
+                        Toast.LENGTH_SHORT).show();
+            }
+            else {
+                SharedPreferences.Editor editor = mSettings.edit();
+                editor.putString(APP_PREFERENCES_NEW_ROOM, bufStr);
+                //Log.wtf("OMG ",bufStr);
+                //Log.wtf("OMG ", mSettings.getString(APP_PREFERENCES_NEW_ROOM, "Null"));
+                editor.apply();
+                finish();
+            }
+        });
     }
 }
